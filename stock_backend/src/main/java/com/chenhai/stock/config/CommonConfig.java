8f80910c1aa645b123e8192e73cfcc71fd326978
youthOfMe <2040690180@qq.com>
2024-05-12 23:11:06 +0800

    feat: 将密码加密器passwordEncoder托管到IOT容器中

--- stock_backend/src/main/java/com/chenhai/stock/config/CommonConfig.java
+++ stock_backend/src/main/java/com/chenhai/stock/config/CommonConfig.java
@@ -1,8 +1,22 @@
 package com.chenhai.stock.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 /**
- * 作者：洋哥
- * 描述：牛逼
+ * 定义公共配置bean
  */
+@Configuration
 public class CommonConfig {
+
+    /**
+     * 定义密码加密的Bean对象
+     * @return
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
