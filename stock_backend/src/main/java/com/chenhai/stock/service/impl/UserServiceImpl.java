4ffd1187325bb5b8053fa4b7d132abcf48c89392
youthOfMe <2040690180@qq.com>
2024-05-12 22:32:47 +0800

    feat: 完善登录业务逻辑

--- stock_backend/src/main/java/com/chenhai/stock/service/impl/UserServiceImpl.java
+++ stock_backend/src/main/java/com/chenhai/stock/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.chenhai.stock.service.impl;
 
+import com.chenhai.stock.content.StockConstant;
 import com.chenhai.stock.mapper.SysUserMapper;
 import com.chenhai.stock.pojo.entity.SysUser;
 import com.chenhai.stock.service.UserService;
@@ -8,8 +9,10 @@ import com.chenhai.stock.vo.res.LoginRespVo;
 import com.chenhai.stock.vo.res.R;
 import com.chenhai.stock.vo.res.ResponseCode;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service("userService")
@@ -17,6 +20,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private SysUserMapper sysUserMapper;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private RedisTemplate redisTemplate;
@@ -47,8 +53,32 @@ public class UserServiceImpl implements UserService {
             return R.error(ResponseCode.CHECK_CODE_ERROR);
         }
         // 判断redis中保存的验证码与输入的验证码是否相同(比较是忽略大小写)
-        // String redisCode = (String) redisTemplate.opsForValue().get()
+        String redisCode = (String) redisTemplate.opsForValue().get(StockConstant.CHECK_PREFIX + loginReqVo.getSessionId());
+        if (StringUtils.isBlank(redisCode)) {
+            // 验证码过期
+            return R.error(ResponseCode.CHECK_CODE_TIMEOUT);
+        }
+        if (!redisCode.equalsIgnoreCase(loginReqVo.getCode())) {
+            // 验证码错误
+            return R.error(ResponseCode.CHECK_CODE_ERROR);
+        }
 
-        return null;
+        // 2. 根据用户名去数据库查询用户信息, 获取密码的明文
+        SysUser dbUser = sysUserMapper.findUserInfoByUserName(loginReqVo.getUsername());
+        if (dbUser == null) {
+            // 用户不存在
+            return R.error(ResponseCode.USERNAME_OR_PASSWORD_ERROR);
+        }
+
+        // 3. 调用密码匹配器匹配输入的明文密码和数据库的密文密码
+        if (!passwordEncoder.matches(loginReqVo.getPassword(), dbUser.getPassword())) {
+            return R.error(ResponseCode.USERNAME_OR_PASSWORD_ERROR);
+        }
+
+        // 4. 响应
+        LoginRespVo respVo = new LoginRespVo();
+        BeanUtils.copyProperties(dbUser, respVo);
+
+        return R.ok(respVo);
     }
 }
