aacb095fffee57798021b580de31c591c4879cf1
youthOfMe <2040690180@qq.com>
2024-05-12 21:39:57 +0800

    feat: 封装响应对象

---
+++ stock_backend/src/main/java/com/chenhai/stock/vo/req/LoginReqVo.java
@@ -0,0 +1,34 @@
+package com.chenhai.stock.vo.req;
+
+import lombok.Data;
+
+/**
+ * @author by itheima
+ * @Date 2021/12/30
+ * @Description 登录请求vo
+ */
+@Data
+// @ApiModel
+public class LoginReqVo {
+    /**
+     * 用户名
+     */
+    // @ApiModelProperty(value = "用户名")
+    private String username;
+    /**
+     * 密码
+     */
+    // @ApiModelProperty(value = "明文密码")
+    private String password;
+    /**
+     * 验证码
+     */
+    // @ApiModelProperty(value = "验证码")
+    private String code;
+
+    /**
+     * 会话ID
+     */
+    // @ApiModelProperty(value = "会话ID")
+    private String sessionId;
+}
